1f81cf080b21a092d2d2b7583a1e39aae7062297
Abhishek <abhishekparmar162@gmail.com>
2023-02-03 19:19:30 +0530

    modified signup page ui

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -4,8 +4,8 @@ export default function Login() {
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
-                    Sign in
+                <h1 className="text-3xl font-semibold text-center text-red-600 uppercase">
+                    Sign UP
                 </h1>
                 <form className="mt-6">
                     <div className="mb-2">
@@ -32,14 +32,9 @@ export default function Login() {
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
-                    <a
-                        href="#"
-                        className="text-xs text-purple-600 hover:underline"
-                    >
-                        Forget Password?
-                    </a>
+                    
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-purple-600">
                             Login
                         </button>
                     </div>
@@ -65,7 +60,7 @@ export default function Login() {
 
                 <p className="mt-8 text-xs font-light text-center text-gray-700">
                     {" "}
-                    Don't have an account?{" "}
+                    Already have an account?{" "}
                     <a
                         href="#"
                         className="font-medium text-red-600 hover:underline"
