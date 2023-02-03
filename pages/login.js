24dcd2e9a353a1df9fdad963944f64c1ce429f06
Abhishek <abhishekparmar162@gmail.com>
2023-02-03 19:34:04 +0530

    added useState

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
-import React from 'react';
+import React,{useState} from 'react';
 
 export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    console.log(email)
+    console.log(password)
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
@@ -17,6 +21,8 @@ export default function Login() {
                             Email
                         </label>
                         <input
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
                             type="email"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
@@ -24,11 +30,14 @@ export default function Login() {
                     <div className="mb-2">
                         <label
                             for="password"
+                            v
                             className="block text-sm font-semibold text-gray-800"
                         >
                             Password
                         </label>
                         <input
+                        value={password}
+                        onChange={e=>setPassword(e.target.value)}
                             type="password"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
