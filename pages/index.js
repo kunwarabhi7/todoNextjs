2f4cade2f7c9636f8cf51dc4b9e88443d30a79d6
Abhishek <abhishekparmar162@gmail.com>
2023-02-04 16:43:39 +0530

    added todo ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [todo, setTodo] = useState("")
   return (
     <>
       <Head>
@@ -9,8 +11,48 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div>
-      Todo App
+      <div className="w-full h-screen bg-gray-100 pt-8">
+        <div className="bg-white p-3 max-w-md mx-auto">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold">ToDo App</h1>
+                <div className="mt-4 flex">
+                    <input
+                    value={todo}
+                    onChange={e=>setTodo(e.target.value)}
+                        className="w-80 border-b-2 border-gray-500 text-black"
+                        type="text" placeholder="Enter your task here" 
+                    />
+                    <button
+                        className="ml-2 border-2 border-green-500 p-2 text-green-500 hover:text-white hover:bg-green-500 rounded-lg flex"
+                    >   
+                        <svg className="h-6 w-6"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="9" y1="12" x2="15" y2="12" />  <line x1="12" y1="9" x2="12" y2="15" /></svg>
+                        <span>Add</span>
+                    </button>
+                </div>        
+            </div>
+            <div className="mt-8">
+                <ul>
+                        <li className="p-2 rounded-lg" >
+                            <div className="flex align-middle flex-row justify-between">
+                                <div className="p-2">
+                                    <input type="checkbox" className="h-6 w-6 " value="true" />
+                                </div>
+                                <div className="p-2">
+                                    <p className="text-lg line-through text-gray-400">Cook maggie</p>
+                                </div>
+                                <button 
+                                    className="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                                    <svg className="h-6 w-6 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="15" y1="9" x2="9" y2="15" />  <line x1="9" y1="9" x2="15" y2="15" /></svg>
+                                    <span>Remove</span>
+                                </button>
+                            </div>
+                            <hr className="mt-2"/>
+                        </li>
+                       
+                </ul>
+            </div>
+           
+        </div>    
     </div>
     </>
   )
