8c4858815f43daa9acda1797788fa813669c51fa
Abhishek <abhishekparmar162@gmail.com>
2023-02-05 20:53:45 +0530

    fix css

--- components/TodosList.jsx
+++ components/TodosList.jsx
@@ -15,7 +15,7 @@ const TodosList = ({todo,isCompleted}) => {
                             <button 
                                 className="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
                                 <svg className="h-6 w-6 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="15" y1="9" x2="9" y2="15" />  <line x1="9" y1="9" x2="15" y2="15" /></svg>
-                                <span>Remove</span>
+                                <span>Removee</span>
                             </button>
                         </div>
                         <hr className="mt-2"/>
